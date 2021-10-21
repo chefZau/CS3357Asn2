4de5691afc0bacb1442d95158ca9e0a71d64b872
Luca(MingCong) Zhou <zhoumingcong6366@gmail.com>
2021-10-21 15:02:27 -0400

    fix: prints msg only when registration success

--- server.py
+++ server.py
@@ -44,8 +44,6 @@ def acceptWrapper(sock):
     # accept connection
     conn, addr = sock.accept()
 
-    print(f'Accepted connection from client address: {addr}')
-
     conn.setblocking(False)
 
     # retrieves the username from the client
@@ -67,7 +65,9 @@ def acceptWrapper(sock):
     conn.sendall(controlMsg.encode(FORMAT))
 
     if validRegistration:
-
+        
+        print(f'Accepted connection from client address: {addr}')
+        
         print(
             f'Connection to client estatblished, waiting to reveive messages from user "{username}" ... ')
 
